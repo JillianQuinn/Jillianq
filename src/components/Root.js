a3855b30c0b9d8d3d0ce54c96f8ba666b9d51158
JillianQuinn <jillian@jillianq.com>
2021-08-23 19:31:03 -0700

    track

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,20 +1,20 @@
-import React from 'react';
+import React from "react";
 
 import { Provider } from "react-redux";
 import store from "../redux/store";
 
-import Routes from './Routes'
+import Routes from "./Routes";
 
-import ReactGA from 'react-ga';
-ReactGA.initialize('UA-50551684-4');
+import ReactGA from "react-ga";
+// ReactGA.initialize("G-HCVRBPMW6H");
+ReactGA.initialize("205715074");
 
 const Root = () => {
-
     return (
         <Provider store={store}>
             <Routes />
         </Provider>
     );
-}
+};
 
 export default Root;
